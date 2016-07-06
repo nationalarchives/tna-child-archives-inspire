abca4e7541eda9d602b5eeb321119b39f316314e
Punal Chotrani <punalsc@gmail.com>
2016-07-06 18:55:57 +0100

    adding carousel to the template

--- archives-inspired.php
+++ archives-inspired.php
@@ -22,7 +22,7 @@ get_header(); ?>
             </div>
         </div>
     </div>
-    <main id class="content-area" role="main">
+    <main class="content-area" role="main">
         <div class="container">
             <section> <!--Main section-->
                 <div class="row">
@@ -46,47 +46,105 @@ get_header(); ?>
             </section><!--End Main section-->
         </div><!--End container-->
         <section class="tabs"><!--Tabs sections-->
-            <div class="row">
-                <div class="col-md-12">
-                    <ul class="nav nav-pills nav-justified">
-                        <li class="active"><a href="#1" data-toggle="tab">Goverment</a></li>
-                        <li><a href="#2" data-toggle="tab">Public</a></li>
-                        <li><a href="#3" data-toggle="tab">Archive sector</a></li>
-                        <li><a href="#4" data-toggle="tab">Research</a></li>
-                        <li><a href="#5" data-toggle="tab">Digital</a></li>
-                    </ul>
-                    <div class="container">
+            <ul class="nav nav-pills nav-justified">
+                <li class="active"><a href="#1" data-toggle="tab">Goverment</a></li>
+                <li><a href="#2" data-toggle="tab">Public</a></li>
+                <li><a href="#3" data-toggle="tab">Archive sector</a></li>
+                <li><a href="#4" data-toggle="tab">Research</a></li>
+                <li><a href="#5" data-toggle="tab">Digital</a></li>
+            </ul>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
                         <div class="tab-content ">
                             <div class="tab-pane active" id="1">
-                                    <div class="col-md-6">
-                                        <p>
-                                            We will provide expert advice and scrutiny to government, making sure that the record survives and thrives.
-                                        </p>
-                                        <p>
-                                            We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="tab_img"></div>
-                                    </div>
+                                <div class="col-md-6">
+                                    <p class="highlight-p">
+                                        We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives.
+                                    </p>
+                                    <p>
+                                        We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="tab_img"></div>
+                                </div>
                             </div>
                             <div class="tab-pane" id="2">
-                                <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+                                <div class="col-md-6">
+                                    <p class="highlight-p">
+                                        We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives.
+                                    </p>
+                                    <p>
+                                        We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="tab_img"></div>
+                                </div>
                             </div>
                             <div class="tab-pane" id="3">
-                                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                                <div class="col-md-6">
+                                    <p class="highlight-p">
+                                        We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives.
+                                    </p>
+                                    <p>
+                                        We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="tab_img"></div>
+                                </div>
                             </div>
                             <div class="tab-pane" id="4">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                                <div class="col-md-6">
+                                    <p class="highlight-p">
+                                        We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives.
+                                    </p>
+                                    <p>
+                                        We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="tab_img"></div>
+                                </div>
                             </div>
                             <div class="tab-pane" id="5">
-                                <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                                <div class="col-md-6">
+                                    <p class="highlight-p">
+                                        We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives.
+                                    </p>
+                                    <p>
+                                        We are both the custodian of the public record and the government’s expert in the management, preservation and use of information. We are uniquely placed by virtue of our history, responsibilities and expertise to provide trusted and independent advice and services across government and the wider public sector.
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="tab_img"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section><!--End tabs section-->
+        <section class="carousel"><!--Carousel section-->
+            <div class="row">
+                <div class="col-md-6 carousel-text">
+                    <p>
+                        Archives are bursting with valuable information and insights. But finding the right information can sometimes take many years of painstaking work. How can our research help researchers everywhere?
+                    </p>
+                    <p>
+                        Traces through time is our ground-breaking international collaborative research project inspired by archives.
+                    </p>
+                    <p>
+                        This is just the beginning of a revolution in research technology.
+                    </p>
+                </div>
+                <div class="col-md-6 carousel-img">
+                    <span>Some text</span>
+                </div>
+            </div>
+        </section><!--End Carousel section-->
     </main>
 </div>
 <?php get_footer(); ?>
