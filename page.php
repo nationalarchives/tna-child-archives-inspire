57d404ff1e2ed3860f57f5be4366c5036f566b6d
Punal Chotrani <punalsc@gmail.com>
2016-08-02 14:29:27 +0100

    removed page title from secondary navigation.

--- page.php
+++ page.php
@@ -2,25 +2,25 @@
 /*
 Template Name: Archives inspire
 */
-get_header(); ?>
+get_header();
+?>
 <div class="a-i">
     <div class="banner" role="banner" style="background-image: url('<?php
     $page_id = $post->ID;
     $image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
-    $childimage = wp_get_attachment_image_src( get_post_thumbnail_id( $post->post_parent ), 'single-post-thumbnail' );
+    $childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
     if (has_post_thumbnail($page_id)) {
         echo make_path_relative($image[0]);
-    }
-    elseif (is_page($page_id)) {
+    } elseif (is_page($page_id)) {
         echo make_path_relative($childimage[0]);
     }
     ?>')">
-    <?php get_template_part('breadcrumb'); ?>
+        <?php get_template_part('breadcrumb'); ?>
         <div class="heading-banner text-left">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="super-heading"><?php the_title(); ?></h1>
+                        <h1 class="super-heading"><?php echo get_the_title(); ?></h1>
                         <?php $sub_heading = get_post_meta($post->ID, 'sub_heading_sub_heading', true);
                         if ($sub_heading) : ?>
                             <h2 class="super-heading"><?php echo $sub_heading; ?></h2>
@@ -33,7 +33,7 @@ get_header(); ?>
     <main class="content-area" role="main">
         <div class="container">
             <?php
-            $args = array('category_name' => 'navigation', 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order');
+            $args = array('category_name' => 'navigation', 'post_type' => 'page','post__not_in' => array($page_id),'order' => 'ASC', 'orderby' => 'menu_order');
             $the_query = new WP_Query($args);
             if ($the_query->have_posts())   : ?>
                 <div class="breadcrumb">
@@ -41,8 +41,13 @@ get_header(); ?>
                         <div class="col-md-12">
                             <ul class="list-inline">
                                 <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                                    <li><a href="<?php echo make_path_relative(get_page_link()); ?>" title="<?php the_title_attribute(); ?>"
-                                           rel="bookmark"><?php the_title(); ?></a></li>
+                                    <li>
+                                        <a href="<?php echo make_path_relative(get_page_link()); ?>"
+                                               title="<?php the_title_attribute(); ?>"
+                                               rel="bookmark"><?php the_title(); ?>
+                                        </a>
+                                    </li>
+
                                 <?php endwhile; ?>
                             </ul>
                         </div>
@@ -80,9 +85,10 @@ get_header(); ?>
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                             <?php the_content(); ?>
                             <?php if (in_category('form')) : ?>
-                                <iframe src="https://r1.dotmailer-surveys.com/b2mand7-0d1xfr61" frameborder="0" scrolling="no"></iframe>
+                                <iframe src="https://r1.dotmailer-surveys.com/b2mand7-0d1xfr61" frameborder="0"
+                                        scrolling="no"></iframe>
                             <?php endif; ?>
-                            <?php if (in_category('case-study') &&  $post->post_parent==$pageID ) : ?>
+                            <?php if (in_category('case-study') && $post->post_parent == $pageID) : ?>
                                 <h2>Case studies</h2>
                             <?php endif; ?>
                         <?php endwhile; endif; ?>
@@ -145,7 +151,8 @@ get_header(); ?>
                                                     $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
                                                     $thumb_url = $thumb_url_array[0];
                                                     ?>
-                                                    <img src="<?php echo make_path_relative($thumb_url) ?>" alt="<?php the_title(); ?>" class="img-responsive">
+                                                    <img src="<?php echo make_path_relative($thumb_url) ?>"
+                                                         alt="<?php the_title(); ?>" class="img-responsive">
                                                 </div>
                                             </div>
                                         <?php elseif (!empty($child_video)) : ?>
